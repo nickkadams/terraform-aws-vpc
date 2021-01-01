dfc2b0912ac3aa4136c08efcc17f551c05560e87
Nick Adams <nickkadams@users.noreply.github.com>
2021-01-01 12:07:21 -0500

    Remove http from versions.tf

--- versions.tf
+++ versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.22.0"
     }
-    #    http = {
-    #      source  = "hashicorp/http"
-    #      version = "~> 1.2.0"
-    #    }    
   }
   required_version = ">= 0.14"
 }

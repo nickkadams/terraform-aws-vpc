6f44c78cf410d4a5f393aceded47838469e95f0b
Nick Adams <nickkadams@users.noreply.github.com>
2021-01-02 08:55:50 -0500

    Add id output to terraform-state S3 bucket

--- terraform-state/outputs.tf
+++ terraform-state/outputs.tf
@@ -1,9 +1,14 @@
+output "s3_bucket_id" {
+  description = "The ID of the S3 bucket."
+  value       = aws_s3_bucket.terraform_state.id
+}
+
 output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
+  description = "The ARN of the S3 bucket."
   value       = aws_s3_bucket.terraform_state.arn
 }
 
 output "dynamodb_table_name" {
-  description = "The name of the DynamoDB table"
+  description = "The name of the DynamoDB table."
   value       = aws_dynamodb_table.terraform_locks.name
 }

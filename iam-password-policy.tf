6e02222f0400d415077a5f871a7c119071ee17a3
Nick Adams <nickkadams@users.noreply.github.com>
2021-05-20 15:40:16 -0400

    Add EBS volume encryption policy and bump IAM password to 14 characters

--- iam-password-policy.tf
+++ iam-password-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 8
+  minimum_password_length        = 14
   require_uppercase_characters   = true
   require_lowercase_characters   = true
   require_numbers                = true
